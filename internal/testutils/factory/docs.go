67161f29b18aeece92b5df7da0a279f685c4b532
blank <hayaken3007@i.softbank.jp>
2024-05-02 22:14:52 +0900

    aqua checksum update #1

---
+++ internal/testutils/factory/docs.go
@@ -0,0 +1,2 @@
+// Package factory provides a way to create test data for the tests.
+package factory
--- internal/testutils/factory/factory.go
+++
@@ -1,8 +0,0 @@
-// Package factory provides a way to create test data for the tests.
-package factory
-
-// DataFactory is an interface for generating test data.
-type DataFactory interface {
-	// Generate generates test data.
-	Generate() error
-}
